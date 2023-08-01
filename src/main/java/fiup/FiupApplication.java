75f3fac13cc699e7ed8b49f2962f047fa8f4d43e
Aleksei Raishin <ighn215@gmail.com>
2023-08-02 03:25:06 +0700

    Init

---
+++ src/main/java/fiup/FiupApplication.java
@@ -0,0 +1,13 @@
+package fiup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fiup/FiupApplicationTests.java
@@ -0,0 +1,13 @@
+package fiup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
